67b94351f11271ed30911efbb1194f40fd661a54
Dylan Caldwell <dylan.caldwell35@gmail.com>
2020-06-02 22:40:38 -0400

    added mobile styles

--- index.js
+++ index.js
@@ -26,14 +26,27 @@ const menuLinks = document.querySelectorAll('#sideMenu li a');
 
 hamBtn.addEventListener('click', () => {
   menu.classList.add('active');
+  setTimeout(() => {
+    menuLinks[0].classList.add('link-animation');
+  }, 100);
+  setTimeout(() => {
+    menuLinks[1].classList.add('link-animation');
+  }, 200);
+  setTimeout(() => {
+    menuLinks[2].classList.add('link-animation');
+  }, 300);
 });
 
 exitBtn.addEventListener('click', () => {
   menu.classList.remove('active');
+  menuLinks.forEach((link, i) => {
+    link.classList.remove('link-animation');
+  });
 });
 
 menuLinks.forEach((link) => {
   link.addEventListener('click', () => {
     menu.classList.remove('active');
+    link.classList.remove('link-animation');
   });
 });
